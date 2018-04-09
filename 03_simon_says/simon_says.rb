5cd1f2161bd7bf6039a5df6eb335deacf89a4e9c
Selma <37739380+Asakab@users.noreply.github.com>
2018-04-09 18:05:43 +0200

    Add files via upload

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,17 @@
-#write your code here
+def echo(hello)
+	hello
+end
+
+def shout(hello)
+	hello.upcase 
+end
+
+def repeat (word, n=2)
+p Array.new(n,word).join(" ")
+end
+
+
+def start_of_word(coucou, n=2)
+coucou.split("").first
+end 
+
