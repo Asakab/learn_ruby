f2a9f4e3cc82c3dcb2dbf863fa02a7997bb30b0f
Selma <37739380+Asakab@users.noreply.github.com>
2018-04-09 18:50:41 +0200

    Add files via upload

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -11,7 +11,16 @@ p Array.new(n,word).join(" ")
 end
 
 
-def start_of_word(coucou, n=2)
-coucou.split("").first
+def start_of_word(coucou, b=2)
+coucou[0, b]
 end 
 
+def first_word(hello)
+	hello.split(" ")[0]
+end
+
+def titleize(jaws)
+	a = jaws.split(" ")
+	a[0].capitalize!
+	
+end
