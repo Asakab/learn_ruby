3cbb6845ad86e768b3e70461fe9e4d7f0a6943ac
Selma <37739380+Asakab@users.noreply.github.com>
2018-04-09 20:08:18 +0200

    Add files via upload

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,20 @@
-#write your code here
+def translate(apple)
+	voyelles = ["a", "e", "i", "o"]
+	consonnes = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "z"]
+	
+	lettres = apple.split(" ")
+
+
+	if apple.start_with?(*voyelles)
+		apple << "ay"
+	else
+		if apple.start_with?(*consonnes)
+			d = apple[0]
+			apple << d
+		apple[0] = ""
+		apple << "ay"
+	
+end
+end
+end
+
